3ccfb45e9c861dfbe9c320a59e84704b3c4c8e3f
ZQ <zhequan7chuang@gmail.com>
2020-12-23 11:23:46 +0800

    calculator class: wrote total, current, future cap and su calculation

--- lib/classes/calculator.dart
+++ lib/classes/calculator.dart
@@ -5,18 +5,47 @@ class Calculator {
   double goalCAP;
   Calculator({@required this.goalCAP});
 
-  double cap(List<Module> mods) {
-    double totalGrade = 0;
-    int completedModsCounter= 0;
+  double futureCap(List<Module> mods) {
+    double totalFutureGrade = 0;
+    double totalCredits = 0;
     for (Module mod in mods) {
-      
+      if (!mod.done) {
         totalGrade += mod.grade;
-        completedModsCounter += 1;
+        totalCredits += mod.credits;
+      }
     }
-    return totalGrade / completedModsCounter;
+    return totalFutureGrade / totalCredits
+  }
+
+  double currentCAP(List<Modules> mods) {
+    double totalCurrentGrade = 0;
+    double totalCredits = 0;
+    for (Module mod in mods) {
+      if (mod.done) {
+        totalGrade += mod.grade;
+        totalCredits += mod.credits;
+      }
+    }
+  }
+
+  double totalCAP(List<Modules> mods) {
+    double totalGrade = 0;
+    int totalCredits = 0;
+    for (Module mod in mods) {
+      totalGrade += mod.grade;
+      totalCredits += mod.credits;
+    }
+    return totalGrade / totalCredits;
   }
 
   bool increaseCAP(double cap) {
     return goalCAP > cap;
+  }
+
+  double capDiffIfSU (Int totalMCs, Int grade, Int credits, double goalCAP){
+    //positive means CAP increase if SU, negative means CAP decrease if SU
+    double totalPoints = goalCAP * totalMCs;
+    double pointsWithoutSU = (totalMCs - credits) * goalCAP + (credits * grade);
+    return (totalPoints - pointsWithoutSU) / totalMCs
   }
 }
